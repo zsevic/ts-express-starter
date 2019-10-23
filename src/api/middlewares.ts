41a97960a2165675438be94f5c760b9e25c2200a
zsevic <zsevic95@gmail.com>
2019-10-24 00:32:34 +0200

    :rocket: Setup middlewares

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,12 +1,23 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import compression from 'compression';
+import helmet from 'helmet';
 import morgan from 'morgan';
 
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
 export default (app) => {
-  app.use(cors());
+  if (isProd) {
+    app.use(compression());
+    app.use(helmet());
+  }
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cors());
 
-  app.use(morgan('dev'));
+  if (isDev) {
+    app.use(morgan('dev'));
+  }
 };
