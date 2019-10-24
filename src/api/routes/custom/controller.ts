f68ed7735f448f0d32df9c65b88534da3095b0e2
zsevic <zsevic95@gmail.com>
2019-10-24 12:11:14 +0200

    :bug: Fix validation

--- src/api/routes/custom/controller.ts
+++ src/api/routes/custom/controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import { getCustomList } from 'domain/use_cases/getCustomList';
 
-export async function getCustomListHandler(req: Request, res: Response): Promise<void> {
+export async function getCustomListHandler(req: Request, res: Response): Promise<Response> {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   const customList = await getCustomList();
 
-  res.json(customList);
+  return res.json(customList);
 }
