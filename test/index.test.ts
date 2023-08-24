29a216f58b448d77302ca12e1b4d4a253925025b
zsevic <zsevic95@gmail.com>
2023-08-24 20:21:39 +0200

    Fix tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,21 +5,14 @@ import constants from 'src/config/constants';
 
 const request = supertest(app);
 
-console.log('mongo', constants.MONGODB_URL);
-
 describe('API tests', () => {
   beforeAll(async () => {
     mongoose.set('strictQuery', false);
-    await mongoose.connect(constants.MONGODB_URL, (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-    });
+    await mongoose.connect(constants.MONGODB_URL);
   });
 
-  afterAll(() => {
-    mongoose.connection.close();
+  afterAll(async () => {
+    await mongoose.connection.close();
   });
 
   it('GET /', async () => {
