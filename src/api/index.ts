4a637187284cc7ab6d73cbc70bc59c02b71c47ed
zsevic <zsevic95@gmail.com>
2023-08-24 19:17:18 +0200

    Improve file naming

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import { errors } from 'celebrate';
 import express from 'express';
 import expressOasGenerator from 'express-oas-generator';
 import { isEnv } from 'utils';
-import { errorHandler } from './middlewares/errorHandler';
+import { errorHandler } from './middlewares/error-handler';
 import registerMiddlewares from './middlewares';
 import registerRoutes from './routes';
 
